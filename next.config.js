b17519fa72055b863a0b5abf4389e40418db5a25
gizeli-pereira <gizeli.pereira1@gmail.com>
2023-11-09 13:18:28 -0600

    first

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require("next-pwa");
 
-module.exports = nextConfig
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["imagesvc.meredithcorp.io","links.papareact.com","i.ibb.co", "a0.muscache.com","res.cloudinary.com", "media.giphy.com","static.dezeen.com","cdn.dribbble.com","teahub.io"],
+  },
+  env : {
+    mapbox_key: 'pk.eyJ1Ijoicm96ZW4yMDA3IiwiYSI6ImNrczBvM3kwNTFseGsycXM3dzljOWdlc20ifQ.mazu3J8KWa6Xlix7K4nzoQ'
+  }
+};
